05517c15d6f3e485f82f198cd5337f036cce5911
harshitlegend <95034947+harshitlegend@users.noreply.github.com>
2023-07-31 02:56:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useState , useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { Auth } from "./components/Auth"
 import Cookies from 'universal-cookie';
 import Chat from './components/Chat';
 import { signOut } from 'firebase/auth';
+import { auth } from './firebase-config';
 
 const cookies = new Cookies();
 
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
   const [room, setRoom] = useState(null);
-  const roomInputRef=useRef(null);
+  const roomInputRef = useRef(null);
+  const signUserOut = async ()=>{
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setIsAuth(false);
+    setRoom(null);
+  }
   if (!isAuth) {
     return (
       <div className="App">
@@ -18,29 +25,47 @@ function App() {
     );
 
   }
-  return <div>
+  return <>
     {room ?
       (
+        
+<>
         <Chat room={room}></Chat>
+        
+        
+          </>
+  
+        
+
       ) : (
-        <div style={{display:'flex' , marginTop:'10%'}}>
+        <div style={{ display: 'flex', marginTop: '10%' }}>
 
-         <div style={{border:'1px solid black' , height:'100%' , width:'50%' , padding:'2%' , 
-          boxShadow: "5px 5px 5px lightblue", margin:'auto' 
+          <div style={{
+            border: '1px solid black', height: '100%', width: '50%', padding: '2%',
+            boxShadow: "5px 5px 5px lightblue", margin: 'auto'
           }}>
-        <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" style={{fontFamily:'sans-serif'}} className="form-label"><h1>Enter Room Name:</h1></label>
-    <input ref={roomInputRef} type="text" className="form-control"/>
-  </div>
-  <button onClick={()=>setRoom(roomInputRef.current.value)} type="button" class="btn btn-dark">Enter Chat</button>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" style={{ fontFamily: 'sans-serif' }} className="form-label"><h1>Enter Room Name:</h1></label>
+              <input ref={roomInputRef} type="text" className="form-control" />
+            </div>
+            <button onClick={() => setRoom(roomInputRef.current.value)} type="button" class="btn btn-dark">Enter Chat</button>
+           
 
-      </div> 
+
+          </div>
+
+
+        
+
+
         </div>
-    
-      )
-      }
-    
-      </div>
+       
+      )}
+      <div style={{display:'flex' , justifyContent:'center'}}>
+            <button onClick={signUserOut} style={{marginTop:'5%'}} type="button" class="btn btn-primary">Sign Out</button>
+          </div>
+
+  </>
 
 }
 
